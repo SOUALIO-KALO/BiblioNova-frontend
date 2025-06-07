ee78e84aab3a18313b7ccb64c348e15f740b9f1a
KALO SOUALIO <kalosoualio6@example.com>
2025-06-07 09:11:05 +0000

    Mise à jour des dépendances et modifications du slice books

--- src/redux/features/booksSlice.ts
+++ src/redux/features/booksSlice.ts
@@ -15,7 +15,7 @@ const initialState: IBooksState = {
   categories: [],
 };
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 // Action asynchrone pour récupérer les livres
 export const fetchBooks = createAsyncThunk<
